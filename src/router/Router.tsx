e2b0f6cdf9fb29ee08c60094ad7fc35b2e167b1b
YouYou94 <ybh4115@gmail.com>
2023-03-15 18:09:57 +0900

    코드 정리

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Menu } from '../components';
 import { Home, User } from '../pages';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Menu />
       <Routes>
         <Route path="/*" element={<Navigate to="/noteapp" />} />
         <Route path="/noteapp" element={<Home />} />
