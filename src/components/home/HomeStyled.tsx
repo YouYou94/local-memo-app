f178b2664eede4231f3a815e28846836823885cf
YouYou94 <ybh4115@gmail.com>
2023-03-17 20:38:02 +0900

    Style: Enrol 스타일 추가

--- src/components/home/HomeStyled.tsx
+++ src/components/home/HomeStyled.tsx
@@ -7,7 +7,7 @@ export const Box = styled.div<{ align: string }>`
   justify-content: center;
   align-items: flex-${(prop) => prop.align};
 
-  padding: 2rem;
+  padding: 0.5rem;
 `;
 
 /* Clock */
@@ -32,3 +32,30 @@ export const TimeSpan = styled.span`
 `;
 
 export const ColonSpan = styled.span``;
+
+/* Enrol */
+export const EnrolBox = styled.div`
+  width: 100%;
+  height: 5rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const EnrolInput = styled.input`
+  width: 30rem;
+  height: 3rem;
+
+  border: none;
+  border-bottom: 1px solid black;
+  padding: 0 1rem;
+
+  outline: none;
+
+  font-size: 1.4rem;
+
+  text-align: center;
+
+  background-color: transparent;
+`;
