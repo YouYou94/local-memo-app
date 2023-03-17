64379277dee6d897b6025e393bfb1fcf9dc83a1c
YouYou94 <ybh4115@gmail.com>
2023-03-17 20:37:52 +0900

    Feat: 사용자 이름 등록 기능 구현

--- src/components/home/enrol/Enrol.tsx
+++ src/components/home/enrol/Enrol.tsx
@@ -1,7 +1,43 @@
-import { Box } from '../HomeStyled';
+import { useState } from 'react';
+import { Box, EnrolBox, EnrolInput } from '../HomeStyled';
 
 const START = 'start';
 
 export const Enrol = () => {
-  return <Box align={START}>등록</Box>;
+  const [name, setName] = useState<string>('');
+
+  const onChangeNameInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    setName(value);
+  };
+
+  const onKeyPressNameInput = (
+    event: React.KeyboardEvent<HTMLInputElement>,
+  ) => {
+    const { key } = event;
+
+    if (key === 'Enter') {
+      if (!name) {
+        alert('이름을 입력해주세요!');
+        return;
+      }
+
+      localStorage.setItem('user', name);
+      setName('');
+    }
+  };
+
+  return (
+    <Box align={START}>
+      <EnrolBox>
+        <EnrolInput
+          value={name}
+          onChange={onChangeNameInput}
+          onKeyPress={onKeyPressNameInput}
+          placeholder="What your name?"
+        />
+      </EnrolBox>
+    </Box>
+  );
 };
