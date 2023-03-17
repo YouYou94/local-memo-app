fce63dc5ad899922cf2d4c82c7fdd2beca8dec54
YouYou94 <ybh4115@gmail.com>
2023-03-17 16:19:02 +0900

    Add: Clock Components 생성

--- src/components/home/clock/Clock.tsx
+++ src/components/home/clock/Clock.tsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from 'react';
 import { Box } from '../HomeStyled';
 
 const END = 'end';
 
 export const Clock = () => {
-  return <Box align={END}>시계</Box>;
+  const [time, setTime] = useState<any>(new Date());
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+
+  return <Box align={END}>{time.getSeconds()}</Box>;
 };
