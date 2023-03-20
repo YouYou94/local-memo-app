29e9bb4301113f7a88728ce3c365c1aea9f359bc
YouYou94 <ybh4115@gmail.com>
2023-03-20 17:58:31 +0900

    방향성 정리중...

--- src/containers/home/HomeContainer.tsx
+++ src/containers/home/HomeContainer.tsx
@@ -5,6 +5,9 @@ const COLUMN = 'column';
 
 const HomeContainer = () => {
   // 유저 정보 체크
+  // 메뉴가 탭 화면 형식
+  // Home은 시간과 노트 관리 화면을 보여준다.
+  // Write 페이지가 따로 존재해야할듯
   const [user, setUser] = useState<string>(localStorage.getItem('user') || '');
 
   console.log(user);
