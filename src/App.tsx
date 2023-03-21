f3ce9f8a5d3a8b5a663449d547c9f650fd4e5603
YouYou94 <ybh4115@gmail.com>
2023-03-21 15:13:05 +0900

    Mod: 메뉴 수정하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { RecoilRoot } from 'recoil';
+import { useState } from 'react';
 import { Menu } from './components';
 import './style/font.css';
 import './App.css';
 
 function App() {
+  const [tap, setTap] = useState<number>(0);
+
   return (
     <RecoilRoot>
       <div className="App">
-        <Menu />
+        <Menu setTap={setTap} />
       </div>
     </RecoilRoot>
   );
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -4,15 +4,21 @@ import { Box, DisplayButton, List, MenuIcon, MenuItem } from './MenuStyled';
 import HOME from '../../assets/image/home.png';
 import DARKMODE from '../../assets/image/darkmode.png';
 import Add from '../../assets/image/add.png';
+import Manage from '../../assets/image/manage.png';
 
-export const Menu = () => {
+export const Menu = ({ setTap }: any) => {
   const [mode, setMode] = useRecoilState<string>(modeState);
 
-  const onClickHomeMenu = () => {};
+  const onClickModeChangeMenu = () => {
+    if (mode === 'bright') setMode('dark');
+    else setMode('bright');
+  };
 
-  const onClickModeChanger = () => {};
+  const onClickHomeMenu = () => setTap(0);
 
-  const onClickAddNote = () => {};
+  const onClickAddMenu = () => setTap(1);
+
+  const onClickManageMenu = () => setTap(2);
 
   return (
     <Box mode={mode}>
@@ -22,14 +28,17 @@ export const Menu = () => {
         N<br />U
       </DisplayButton>
       <List>
-        <MenuItem onClick={onClickHomeMenu} title="홈으로가기">
-          <MenuIcon icon={HOME} />
-        </MenuItem>
-        <MenuItem onClick={onClickModeChanger} title="화면대비">
+        <MenuItem onClick={onClickModeChangeMenu} title="화면대비">
           <MenuIcon icon={DARKMODE} mode={mode} />
         </MenuItem>
-        <MenuItem onClick={onClickAddNote} title="노트추가">
+        <MenuItem onClick={onClickHomeMenu} title="메인으로가기">
+          <MenuIcon icon={HOME} />
+        </MenuItem>
+        <MenuItem onClick={onClickAddMenu} title="노트추가하기">
           <MenuIcon icon={Add} />
+        </MenuItem>
+        <MenuItem onClick={onClickManageMenu} title="노트관리하기">
+          <MenuIcon icon={Manage} />
         </MenuItem>
       </List>
     </Box>
