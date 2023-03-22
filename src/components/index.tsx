575bcdb91668c96b299702fee4cae87ce9040861
YouYou94 <ybh4115@gmail.com>
2023-03-22 17:01:53 +0900

    Add: Greeting Components 생성

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -9,5 +9,6 @@ export { Write } from './write/Write';
 /* Home */
 export { Clock } from './home/clock/Clock';
 export { Enrol } from './home/enrol/Enrol';
+export { Greeting } from './home/greeting/Greeting';
 
 export { Manage } from './manage/Manage';
