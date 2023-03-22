9ba0436dffaddaf24adcbb49c59bd9e404082ff4
YouYou94 <ybh4115@gmail.com>
2023-03-22 17:21:07 +0900

    Feat: 글 쓰기 기능 구현

--- src/components/writing/write/Write.tsx
+++ src/components/writing/write/Write.tsx
@@ -1,3 +1,29 @@
-export const Write = () => {
-  return <></>;
+import { Box, Header, Section, WriteArea } from './WriteStyled';
+
+type WriteProps = {
+  state: string;
+  setState: any;
+};
+
+export const Write = ({ state, setState }: WriteProps) => {
+  const handleChangeWriteArea = (
+    event: React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    const { value } = event.target;
+
+    setState(value);
+  };
+
+  return (
+    <Box>
+      <Header>세팅 메뉴</Header>
+      <Section>
+        <WriteArea
+          value={state}
+          onChange={handleChangeWriteArea}
+          placeholder="메모를 작성해주세요...."
+        />
+      </Section>
+    </Box>
+  );
 };
---
+++ src/components/writing/write/WriteStyled.tsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const Box = styled.div`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+
+  padding: 2rem;
+`;
+
+export const Header = styled.header`
+  height: 5rem;
+`;
+
+export const Section = styled.section`
+  flex: 1;
+
+  display: flex;
+`;
+
+export const WriteArea = styled.textarea`
+  flex: 1;
+
+  border: none;
+
+  font-size: 1.3rem;
+
+  background-color: transparent;
+
+  outline: none;
+`;
