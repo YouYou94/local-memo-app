bc44bde99d120e883a5f1a131b9495099410fe65
YouYou94 <ybh4115@gmail.com>
2023-03-29 16:18:12 +0900

    Style: Manage 스타일 수정

--- src/components/manage/ManageStyled.tsx
+++ src/components/manage/ManageStyled.tsx
@@ -19,11 +19,23 @@ export const List = styled.ul`
   gap: 2rem;
 `;
 
-export const Item = styled.li`
+export const Item = styled.li<{ color: string }>`
   width: 15rem;
   height: 20rem;
 
+  display: flex;
+  flex-direction: column;
+
   border: 1px solid rgb(204, 204, 204);
+  padding: 1rem;
+
+  background-color: ${(prop) => prop.color};
 
   cursor: pointer;
+
+  gap: 1rem;
 `;
+
+export const ItemTitle = styled.h2``;
+
+export const ItemMemo = styled.div``;
