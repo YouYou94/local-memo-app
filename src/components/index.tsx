e94369350a7f321ff01ae9e0a99d8f1497586b9f
YouYou94 <ybh4115@gmail.com>
2023-03-30 17:21:12 +0900

    Refactor: 파일 구조 및 네이밍 수정

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,14 +1,12 @@
 /* Common */
 export { Template } from './common/template/Template';
+export { Menu } from './common/menu/Menu';
+export { Clock } from './common/clock/Clock';
 
 /* Components */
-export { Menu } from './common/menu/Menu';
-export { Preview } from './writing/preview/Preview';
-export { Write } from './writing/write/Write';
+/* Writing */
+export { Preview } from './notewriting/preview/Preview';
+export { Write } from './notewriting/write/Write';
 
-/* Home */
-export { Clock } from './home/clock/Clock';
-export { Enrol } from './home/enrol/Enrol';
-export { Greeting } from './home/greeting/Greeting';
-
-export { Manage } from './manage/Manage';
+/* Manager */
+export { Manage } from './notemanager/Manage';
