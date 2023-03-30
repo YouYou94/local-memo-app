1c333aa569165203f810f34ca0dcf874bf807272
YouYou94 <ybh4115@gmail.com>
2023-03-30 12:48:38 +0900

    :fire: HomeContainer

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -1,3 +1,2 @@
-export { default as HomeContainer } from './home/HomeContainer';
 export { default as ManageContainer } from './manage/ManageContainer';
 export { default as WritingContainer } from './writing/WritingContainer';
