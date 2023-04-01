6cd4fdaa426b80d1338d2763b0639fd3c2470121
YouYou94 <ybh4115@gmail.com>
2023-04-01 17:40:29 +0900

    Export: WidgetMain Components Export

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -10,3 +10,10 @@ export { Write } from './notewriting/write/Write';
 
 /* Manager */
 export { Manage } from './notemanager/Manage';
+
+/* Widget */
+export { WeatherBackground } from './widgetmain/weatherbackground/WeatherBackground';
+export { TopBar } from './widgetmain/topbar/TopBar';
+export { Feat } from './widgetmain/feat/Feat';
+export { Search } from './widgetmain/search/Search';
+export { Footer } from './widgetmain/footer/Footer';
