61b7c9b4085ab4392f880aef7131951427716a5d
YouYou94 <ybh4115@gmail.com>
2023-04-02 19:26:00 +0900

    Style: Feat 스타일 수정

--- src/components/widgetmain/feat/FeatStyled.tsx
+++ src/components/widgetmain/feat/FeatStyled.tsx
@@ -2,4 +2,11 @@ import styled from 'styled-components';
 
 export const Box = styled.section`
   flex: 1;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
 `;
+
+export const Title = styled.h1``;
