870f8206052bc1892e6322d57ecaef0e956528ad
YouYou94 <ybh4115@gmail.com>
2023-04-03 20:31:30 +0900

    Feat: 시간 기능 구현

--- src/components/widgetmain/feat/Feat.tsx
+++ src/components/widgetmain/feat/Feat.tsx
@@ -1,9 +1,29 @@
-import { Box, Title } from './FeatStyled';
+import { useEffect, useState } from 'react';
+import { Box, Title, TimeBox, TimeSpan, ColonSpan } from './FeatStyled';
 
 export const Feat = () => {
+  const [time, setTime] = useState<any>(new Date());
+  const hour = String(time.getHours()).padStart(2, '0');
+  const minute = String(time.getMinutes()).padStart(2, '0');
+  const second = String(time.getSeconds()).padStart(2, '0');
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+
   return (
     <Box>
       <Title>MLPW</Title>
+      <TimeBox>
+        <TimeSpan>{hour}</TimeSpan>
+        <ColonSpan>:</ColonSpan>
+        <TimeSpan>{minute}</TimeSpan>
+        <ColonSpan>:</ColonSpan>
+        <TimeSpan>{second}</TimeSpan>
+      </TimeBox>
     </Box>
   );
 };
