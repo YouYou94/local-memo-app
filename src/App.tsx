68488c821487b70a82e573ae4d80a6635f636817
YouYou94 <ybh4115@gmail.com>
2023-04-05 19:18:35 +0900

    Refactor: 코드 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import { RecoilRoot } from 'recoil';
+import MainContainer from './containers/MainContainer';
 import './style/font.css';
 import './App.css';
 
 function App() {
-  /* 크롬 스토리지 */
-  /*
-  useEffect(() => {
-    chrome.storage.sync.get(['manage'], function (items) {
-      if (!items.manage) {
-        chrome.storage.sync.set({ manage: JSON.stringify([]) }, function () {
-          console.log('Value is set to ' + JSON.stringify([]));
-        });
-      }
-    });
-  }, []);
-  */
+  /* 나의 작고 소중한 일기 피드 앱 */
+  /* My Little and Precious Diary Feed App */
+  /* MLPDFeed */
 
   return (
     <RecoilRoot>
-      <div className="App"></div>
+      <div className="App">
+        <MainContainer />
+      </div>
     </RecoilRoot>
   );
 }
