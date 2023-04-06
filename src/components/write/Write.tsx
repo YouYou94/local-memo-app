50d5819fbf23925f477e2b1010f846e1bd1bfaca
YouYou94 <ybh4115@gmail.com>
2023-04-06 20:09:52 +0900

    Feat: Write Header Tag 입력 기능 구현

--- src/components/write/Write.tsx
+++ src/components/write/Write.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import {
   Box,
   WriteBox,
@@ -9,14 +10,46 @@ import {
 } from './WriteStyled';
 
 export const Write = () => {
+  const [tagValue, setTagValue] = useState<string>('');
+  const [tagList, setTagList] = useState<any>([]);
+
+  const handleOnChangeTagInput = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const { value } = event.target;
+
+    setTagValue(value);
+  };
+
+  const handleOnPressKeyTagInput = (
+    event: React.KeyboardEvent<HTMLInputElement>,
+  ) => {
+    const { key } = event;
+
+    if (key === 'Enter') {
+      if (tagValue[0] + tagValue[1] === '# ') {
+        setTagList([...tagList, tagValue]);
+      } else alert('"# 태그" 형식으로 태그를 입력하세요.');
+
+      setTagValue('');
+    }
+  };
+
   return (
     <Box>
       <WriteBox>
         <WriteHeader>
           <UploaderBox></UploaderBox>
           <TagBox>
-            <Tag>#안녕하세요</Tag>
-            <TagInput />
+            {tagList?.map((tag: string, index: number): any => {
+              return <Tag key={index}>{tag}</Tag>;
+            })}
+            <TagInput
+              value={tagValue}
+              onChange={handleOnChangeTagInput}
+              onKeyPress={handleOnPressKeyTagInput}
+              placeholder="# 태그를 입력해주세요."
+            />
           </TagBox>
         </WriteHeader>
       </WriteBox>
