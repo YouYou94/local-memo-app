9aa749008c0446bf7f5b13875baeefb3f57b5c44
YouYou94 <ybh4115@gmail.com>
2023-04-06 19:59:59 +0900

    Add: Write Styled-Components 추가

--- src/components/write/Write.tsx
+++ src/components/write/Write.tsx
@@ -1,5 +1,25 @@
-import { Box } from './WriteStyled';
+import {
+  Box,
+  WriteBox,
+  WriteHeader,
+  UploaderBox,
+  TagBox,
+  Tag,
+  TagInput,
+} from './WriteStyled';
 
 export const Write = () => {
-  return <Box>피드 작성</Box>;
+  return (
+    <Box>
+      <WriteBox>
+        <WriteHeader>
+          <UploaderBox></UploaderBox>
+          <TagBox>
+            <Tag>#안녕하세요</Tag>
+            <TagInput />
+          </TagBox>
+        </WriteHeader>
+      </WriteBox>
+    </Box>
+  );
 };
