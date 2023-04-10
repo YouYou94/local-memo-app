f41c316a364f34f8ed075e537727f3b96a90c6ca
YouYou94 <ybh4115@gmail.com>
2023-04-10 20:22:37 +0900

    Feat: 피드 이미지 조회 테스트

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -14,7 +14,11 @@ import {
 } from './FeedStyled';
 
 export const Feed = () => {
-  const feedList = null;
+  //const feedList = null;
+
+  const feedList = localStorage.getItem('image');
+
+  console.log(feedList);
 
   return (
     <Box>
@@ -31,7 +35,7 @@ export const Feed = () => {
           <ButtonBox>버튼</ButtonBox>
         </FeedHeader>
         <FeedArticle>
-          <FeedImage />
+          <FeedImage src={String(feedList)} />
         </FeedArticle>
         <FeedFooter>일기 내용 들어가기</FeedFooter>
       </FeedBox>
