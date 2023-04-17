8a8b4a65bab62b2c625d39c22fcc21021014b06d
YouYou94 <ybh4115@gmail.com>
2023-04-17 15:58:15 +0900

    Style: 스타일 수정

--- src/components/common/template/TemplateStyled.tsx
+++ src/components/common/template/TemplateStyled.tsx
@@ -4,7 +4,9 @@ export const Layout = styled.div`
   flex: 1;
 
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
+
+  margin-top: 6rem;
 
   color: rgb(0, 0, 0);
   background-color: rgb(255, 255, 255);
