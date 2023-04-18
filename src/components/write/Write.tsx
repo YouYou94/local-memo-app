c8de9ab6bfed9a379c4fb00b8301af96c72b9935
YouYou94 <ybh4115@gmail.com>
2023-04-18 18:29:02 +0900

    Feat: Feed Write C 기능 구현

--- src/components/write/Write.tsx
+++ src/components/write/Write.tsx
@@ -56,6 +56,28 @@ export const Write = ({ setTap }: any) => {
     setContentValue(value);
   };
 
+  const handleOnClickEnrolDiary = () => {
+    if (!tagList || !contentValue) {
+      alert('빈 항목이 있습니다.');
+      return;
+    }
+
+    const feedList = JSON.parse(localStorage.getItem('feed') || '');
+
+    const date = new Date();
+    const nowTime = `${String(date.getFullYear()).substring(2)}.${String(
+      date.getMonth() + 1,
+    ).padStart(2, '0')}.${String(date.getDate()).padStart(2, '0')} ${String(
+      date.getHours() > 12 ? date.getHours() - 12 : date.getHours(),
+    ).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
+
+    const object = { tag: tagList, content: contentValue, time: nowTime };
+
+    localStorage.setItem('feed', JSON.stringify([object, ...feedList]));
+
+    setTap(0);
+  };
+
   return (
     <Box>
       <WriteBox>
@@ -85,7 +107,7 @@ export const Write = ({ setTap }: any) => {
         </Article>
         <Nav>
           <Button onClick={() => setTap(0)}>돌아가기</Button>
-          <Button onClick={() => setTap(0)}>등록하기</Button>
+          <Button onClick={handleOnClickEnrolDiary}>등록하기</Button>
         </Nav>
       </WriteBox>
     </Box>
