462165152a4cde1c034b0078fc7181711ada7662
YouYou94 <ybh4115@gmail.com>
2023-04-24 18:07:07 +0900

    Feat: 피드 등록 색깔 추가

--- src/components/write/WriteFooter.tsx
+++ src/components/write/WriteFooter.tsx
@@ -1,18 +1,20 @@
 import { Footer, Button } from './WriteStyled';
 
 type WriteFooterProps = {
+  color: any;
   tagList: any;
   contentValue: string;
   setTap: any;
 };
 
 export const WriteFooter = ({
+  color,
   tagList,
   contentValue,
   setTap,
 }: WriteFooterProps) => {
   const handleOnClickEnrolDiary = () => {
-    if (!tagList || !contentValue) {
+    if (!tagList || !contentValue || !color) {
       alert('빈 항목이 있습니다.');
       return;
     }
@@ -26,7 +28,12 @@ export const WriteFooter = ({
       date.getHours() > 12 ? date.getHours() - 12 : date.getHours(),
     ).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
 
-    const object = { tag: tagList, content: contentValue, time: nowTime };
+    const object = {
+      color: color.colorRGB,
+      tag: tagList,
+      content: contentValue,
+      time: nowTime,
+    };
 
     localStorage.setItem('feed', JSON.stringify([object, ...feedList]));
 
