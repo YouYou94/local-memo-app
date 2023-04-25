3d13b7820884d5cf638e973be26c7044d16a148b
YouYou94 <ybh4115@gmail.com>
2023-04-25 19:33:58 +0900

    Style: 스타일 수정

--- src/components/feed/FeedStyled.tsx
+++ src/components/feed/FeedStyled.tsx
@@ -3,5 +3,13 @@ import styled from 'styled-components';
 export const Box = styled.article`
   flex: 1;
 
-  display: flex;
+  display: grid;
+`;
+
+export const FeedBox = styled.div`
+  width: 15rem;
+
+  aspect-ratio: 3 / 4;
+
+  border: 1px solid rgb(204, 204, 204);
 `;
