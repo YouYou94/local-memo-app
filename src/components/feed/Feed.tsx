beaa60cc99be35426e3a2377789fdcaab9ae5c2d
YouYou94 <ybh4115@gmail.com>
2023-04-25 19:34:11 +0900

    Add: Feed 조회 구현 중..

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -1,5 +1,22 @@
-import { Box } from './FeedStyled';
+import { useEffect, useState } from 'react';
+import { Box, FeedBox } from './FeedStyled';
 
 export const Feed = () => {
-  return <Box>zzz</Box>;
+  const [feedList, setFeedList] = useState<any>([]);
+
+  useEffect(() => {
+    const storageData = JSON.parse(localStorage.getItem('feed') || '');
+
+    if (storageData) setFeedList(storageData);
+  }, []);
+
+  return (
+    <Box>
+      {feedList?.map((feed: any, index: number) => {
+        const { id, color, tag, content, time } = feed;
+
+        return <FeedBox key={id}></FeedBox>;
+      })}
+    </Box>
+  );
 };
