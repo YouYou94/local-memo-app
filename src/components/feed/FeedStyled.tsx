b94d624e6694ff0738feffa0c1cd2ccae708918d
YouYou94 <ybh4115@gmail.com>
2023-04-26 19:28:17 +0900

    Style: 반응형 grid 레이아웃 구현

--- src/components/feed/FeedStyled.tsx
+++ src/components/feed/FeedStyled.tsx
@@ -4,12 +4,35 @@ export const Box = styled.article`
   flex: 1;
 
   display: grid;
+  justify-content: center;
+
+  padding-bottom: 2rem;
+  gap: 1.5rem;
+
+  @media screen and (min-width: 1440px) {
+    grid-template-columns: repeat(5, 1fr);
+  }
+
+  @media screen and (max-width: 1440px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+
+  @media screen and (max-width: 1024px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media screen and (max-width: 480px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `;
 
 export const FeedBox = styled.div`
-  width: 15rem;
-
   aspect-ratio: 3 / 4;
 
   border: 1px solid rgb(204, 204, 204);
+  border-radius: 0.5rem;
 `;
