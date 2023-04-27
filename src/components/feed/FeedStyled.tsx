3946f61331b690c54c114c8c9178fb559b29d436
YouYou94 <ybh4115@gmail.com>
2023-04-27 21:45:28 +0900

    Style: 피드 스타일 추가

--- src/components/feed/FeedStyled.tsx
+++ src/components/feed/FeedStyled.tsx
@@ -7,7 +7,7 @@ export const Box = styled.article`
   justify-content: center;
 
   padding-bottom: 2rem;
-  gap: 1.5rem;
+  gap: 2rem;
 
   @media screen and (min-width: 1440px) {
     grid-template-columns: repeat(5, 1fr);
@@ -33,6 +33,69 @@ export const Box = styled.article`
 export const FeedBox = styled.div`
   aspect-ratio: 3 / 4;
 
+  display: flex;
+  flex-direction: column;
+
   border: 1px solid rgb(204, 204, 204);
-  border-radius: 0.5rem;
+
+  transition: 0.2s all;
+
+  cursor: pointer;
+
+  :hover {
+    transform: scale(1.05);
+  }
+`;
+
+export const FeedHeader = styled.div`
+  height: 2.5rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 0 1rem;
+`;
+
+export const FeedSettingBox = styled.div`
+  flex: 1;
+`;
+
+export const FeedSettingButton = styled.button`
+  border: none;
+
+  background-color: transparent;
+
+  :hover {
+    color: rgb(105, 165, 255);
+  }
+`;
+
+export const FeedTime = styled.label`
+  font-size: 0.8rem;
+`;
+
+export const FeedContent = styled.div<{ color: string }>`
+  flex: 1;
+
+  padding: 0.5rem;
+
+  background-color: ${(prop) => prop.color};
+
+  white-space: pre-wrap;
+`;
+
+export const FeedFooter = styled.div`
+  height: 2.5rem;
+
+  display: flex;
+  align-items: center;
+
+  padding: 0 1rem;
+
+  gap: 1rem;
+`;
+
+export const FeedTag = styled.label`
+  font-size: 0.8rem;
 `;
