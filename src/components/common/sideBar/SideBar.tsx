e2ed572725e85c70876432324eccb76487b8c03d
YouYou94 <ybh4115@gmail.com>
2023-04-30 21:49:10 +0900

    Feat: 메뉴 클릭 시 다른 화면으로 변경 기능 구현

--- src/components/common/sideBar/SideBar.tsx
+++ src/components/common/sideBar/SideBar.tsx
@@ -1,20 +1,24 @@
 import HOME from '../../../assets/icon/home.png';
-import NOTEADD from '../../../assets/icon/noteadd.png';
-import NOTEEDIT from '../../../assets/icon/noteedit.png';
+import WRITE from '../../../assets/icon/noteadd.png';
+import EDIT from '../../../assets/icon/noteedit.png';
 import { Nav, List, Item, Icon } from './SideBarStyled';
 
-export const SideBar = () => {
+export const SideBar = ({ setTap }: any) => {
+  const handleClickHome = () => setTap(0);
+  const handleClickWrite = () => setTap(1);
+  const handleClickEdit = () => setTap(2);
+
   return (
     <Nav>
       <List>
-        <Item>
+        <Item onClick={handleClickHome}>
           <Icon icon={HOME} />
         </Item>
-        <Item>
-          <Icon icon={NOTEADD} />
+        <Item onClick={handleClickWrite}>
+          <Icon icon={WRITE} />
         </Item>
-        <Item>
-          <Icon icon={NOTEEDIT} />
+        <Item onClick={handleClickEdit}>
+          <Icon icon={EDIT} />
         </Item>
       </List>
     </Nav>
