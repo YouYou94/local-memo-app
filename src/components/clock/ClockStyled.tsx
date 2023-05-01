4d4b7bd871718e607d482fa64a35c8f4a48ca1fc
YouYou94 <ybh4115@gmail.com>
2023-05-01 20:22:38 +0900

    Style: Clock 스타일 추가

--- src/components/clock/ClockStyled.tsx
+++ src/components/clock/ClockStyled.tsx
@@ -4,6 +4,62 @@ export const ClockBox = styled.div`
   flex: 1;
 
   display: flex;
+  align-items: center;
+
+  user-select: none;
+`;
+
+export const TimeBox = styled.div`
+  flex: 1;
+
+  display: flex;
   justify-content: center;
-  align-items: flex-end;
+
+  padding-top: 13rem;
+`;
+
+export const HourMinuteBox = styled.div`
+  width: 20rem;
+  height: 10rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  text-align: center;
+`;
+
+export const AmpmSecondsBox = styled.div`
+  width: 4rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const HourMinuteLabel = styled.label`
+  width: 8rem;
+
+  font-family: 'KonkhmerSleokchher';
+  font-size: 6rem;
+`;
+
+export const DotLabel = styled.label`
+  padding-bottom: 1rem;
+
+  font-family: 'KonkhmerSleokchher';
+  font-size: 6rem;
+`;
+
+export const AmpmLabel = styled.label`
+  border-bottom: 1px solid rgb(0, 0, 0);
+
+  font-family: 'KonkhmerSleokchher';
+  font-size: 2rem;
+`;
+
+export const SecondsLabel = styled.label`
+  font-family: 'KonkhmerSleokchher';
+  font-size: 2rem;
 `;
