1e466e0509cf81a26dc0f2b6168946988407b594
YouYou94 <ybh4115@gmail.com>
2023-05-04 22:03:59 +0900

    Refactor: SideBar 변경

--- src/components/common/sideBar/SideBar.tsx
+++ src/components/common/sideBar/SideBar.tsx
@@ -1,24 +1,24 @@
 import HOME from '../../../assets/icon/home.png';
-import WRITE from '../../../assets/icon/noteadd.png';
-import EDIT from '../../../assets/icon/noteedit.png';
+import READ from '../../../assets/icon/read.png';
+import ADD from '../../../assets/icon/add.png';
 import { Nav, List, Item, Icon } from './SideBarStyled';
 
 export const SideBar = ({ setTap }: any) => {
   const handleClickHome = () => setTap(0);
-  const handleClickWrite = () => setTap(1);
-  const handleClickEdit = () => setTap(2);
+  const handleClickRead = () => setTap(1);
+  const handleClickAdd = () => setTap(2);
 
   return (
     <Nav>
       <List>
-        <Item onClick={handleClickHome}>
+        <Item onClick={handleClickHome} title="홈| 검색">
           <Icon icon={HOME} />
         </Item>
-        <Item onClick={handleClickWrite}>
-          <Icon icon={WRITE} />
+        <Item onClick={handleClickRead} title="조회| 노트 조회 수정 삭제">
+          <Icon icon={READ} />
         </Item>
-        <Item onClick={handleClickEdit}>
-          <Icon icon={EDIT} />
+        <Item onClick={handleClickAdd} title="추가| 노트 추가">
+          <Icon icon={ADD} />
         </Item>
       </List>
     </Nav>
