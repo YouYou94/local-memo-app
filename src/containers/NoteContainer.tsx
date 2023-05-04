c848240e6e46a7a47b7c37744e9af37656061ffa
YouYou94 <ybh4115@gmail.com>
2023-05-05 01:45:26 +0900

    Import: Note Container / Write Note Component

--- src/containers/NoteContainer.tsx
+++ src/containers/NoteContainer.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react';
 import { Note, Template, Write } from '../components';
 
-const NoteContainer = ({ setTap }: any) => {
+const NoteContainer = () => {
+  const [note, setNote] = useState<Object>({});
+  const [isUpdate, setIsUpdate] = useState<boolean>(false);
+
+  console.log(isUpdate);
+
+  useEffect(() => {
+    if (localStorage.getItem('note'))
+      setNote(JSON.parse(localStorage.getItem('note') || ''));
+  }, []);
+
   return (
     <Template>
-      <Note />
+      {isUpdate ? (
+        <Write note={note} setNote={setNote} setIsUpdate={setIsUpdate} />
+      ) : (
+        <Note note={note} setNote={setNote} setIsUpdate={setIsUpdate} />
+      )}
     </Template>
   );
 };
