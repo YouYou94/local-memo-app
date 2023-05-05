39d0715fbe83a2d294c8b0cff0c4331660d0e427
YouYou94 <ybh4115@gmail.com>
2023-05-05 20:12:26 +0900

    Refactor: 사이드 메뉴 변경

--- src/components/common/sideBar/SideBar.tsx
+++ src/components/common/sideBar/SideBar.tsx
@@ -1,12 +1,10 @@
 import HOME from '../../../assets/icon/home.png';
 import READ from '../../../assets/icon/read.png';
-import ADD from '../../../assets/icon/add.png';
 import { Nav, List, Item, Icon } from './SideBarStyled';
 
 export const SideBar = ({ setTap }: any) => {
   const handleClickHome = () => setTap(0);
   const handleClickRead = () => setTap(1);
-  const handleClickAdd = () => setTap(2);
 
   return (
     <Nav>
@@ -14,11 +12,8 @@ export const SideBar = ({ setTap }: any) => {
         <Item onClick={handleClickHome} title="홈| 검색">
           <Icon icon={HOME} />
         </Item>
-        <Item onClick={handleClickRead} title="조회| 노트 조회 수정 삭제">
+        <Item onClick={handleClickRead} title="노트| 추가 조회 수정 삭제">
           <Icon icon={READ} />
-        </Item>
-        <Item onClick={handleClickAdd} title="추가| 노트 추가">
-          <Icon icon={ADD} />
         </Item>
       </List>
     </Nav>
