c1b3eac4f602a7813161094b535642853fd73d2c
YouYou94 <ybh4115@gmail.com>
2023-05-08 14:25:11 +0900

    Add: 마크다운 문법 Component 생성

---
+++ src/components/note/markdown/Markdown.tsx
@@ -0,0 +1,113 @@
+import { Fragment } from 'react';
+import {
+  Box,
+  Span,
+  Br,
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  ListSpan,
+  ListDot,
+  Line,
+  ItalicSpan,
+  ThroughSpan,
+} from './MarkdownStyled';
+
+export const Markdown = ({ content }: any) => {
+  return (
+    <Box>
+      {content.split('\n').map((v: string, index: number) => {
+        let value = null;
+        /* ──────────────────────────────────────── */
+        /* H1 ~ H6 */
+        if (v.substring(0, 7) === '###### ') value = <H6>{v.substring(7)}</H6>;
+        else if (v.substring(0, 6) === '##### ')
+          value = <H5>{v.substring(6)}</H5>;
+        else if (v.substring(0, 5) === '#### ')
+          value = <H4>{v.substring(5)}</H4>;
+        else if (v.substring(0, 4) === '### ')
+          value = <H3>{v.substring(4)}</H3>;
+        else if (v.substring(0, 3) === '## ') value = <H2>{v.substring(3)}</H2>;
+        else if (v.substring(0, 2) === '# ') value = <H1>{v.substring(2)}</H1>;
+        /* ──────────────────────────────────────── */
+        /* LIST */ else if (v.substring(0, 2) === '- ')
+          value = (
+            <ListSpan>
+              <ListDot> · </ListDot>
+              {v.substring(2)}
+              <Br />
+            </ListSpan>
+          );
+        /* ──────────────────────────────────────── */
+        /* LINE */ else if (
+          v.substring(0, 3) === '---' ||
+          v.substring(0, 3) === '___' ||
+          v.substring(0, 3) === '***'
+        )
+          value = <Line />;
+        /* ──────────────────────────────────────── */
+        /* ITALIC */ else if (v.includes('_'))
+          value = (
+            <Span>
+              {v
+                .replaceAll('_', '〃_〃')
+                .split('〃')
+                .map((element, index) => {
+                  let italicValue = null;
+
+                  if (element === '') italicValue = null;
+                  else if (
+                    v.replaceAll('_', '〃_〃').split('〃')[index - 1] === '_' &&
+                    v.replaceAll('_', '〃_〃').split('〃')[index + 1] === '_'
+                  )
+                    italicValue = <ItalicSpan>{element}</ItalicSpan>;
+                  else if (element === '_') italicValue = null;
+                  else italicValue = element;
+
+                  return italicValue;
+                })}
+              <Br />
+            </Span>
+          );
+        /* ──────────────────────────────────────── */
+        /* LINE THROUGH */ else if (v.includes('~~'))
+          value = (
+            <Span>
+              {v
+                .replaceAll('~~', '〃~~〃')
+                .split('〃')
+                .map((element, index) => {
+                  let italicValue = null;
+
+                  if (element === '') italicValue = null;
+                  else if (
+                    v.replaceAll('~~', '〃~~〃').split('〃')[index - 1] ===
+                      '~~' &&
+                    v.replaceAll('~~', '〃~~〃').split('〃')[index + 1] === '~~'
+                  )
+                    italicValue = <ThroughSpan>{element}</ThroughSpan>;
+                  else if (element === '~~') italicValue = null;
+                  else italicValue = element;
+
+                  return italicValue;
+                })}
+              <Br />
+            </Span>
+          );
+        /* ──────────────────────────────────────── */
+        /* DEFAULT */ else
+          value = (
+            <Span>
+              {v}
+              <Br />
+            </Span>
+          );
+
+        return <Fragment key={index}>{value}</Fragment>;
+      })}
+    </Box>
+  );
+};
---
+++ src/components/note/markdown/MarkdownStyled.tsx
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+
+export const Box = styled.div`
+  flex: 1;
+  white-space: pre-wrap;
+`;
+
+export const Span = styled.span`
+  line-height: 0%;
+`;
+export const Br = styled.br``;
+
+/* H1 ~ H6 */
+export const H1 = styled.h1`
+  font-size: 2rem;
+`;
+export const H2 = styled.h2`
+  font-size: 1.5rem;
+`;
+export const H3 = styled.h3`
+  font-size: 1.17rem;
+`;
+export const H4 = styled.h4`
+  font-size: 1rem;
+`;
+export const H5 = styled.h5`
+  font-size: 0.83rem;
+`;
+export const H6 = styled.h6`
+  font-size: 0.67rem;
+`;
+
+/* List */
+export const ListSpan = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+export const ListDot = styled.span`
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+
+/* Line */
+export const Line = styled.div`
+  margin: 0.5rem 0;
+  border: 1px solid rgb(204, 204, 204);
+`;
+
+/* Italic */
+export const ItalicSpan = styled.span`
+  font-style: italic;
+`;
+
+/* Line Through */
+export const ThroughSpan = styled.span`
+  text-decoration: line-through;
+`;
