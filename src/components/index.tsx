2774dc261997b8d901613d5b83afe59a262bbc61
YouYou94 <ybh4115@gmail.com>
2023-05-17 17:12:02 +0900

    Export.

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,11 @@
 /* Template */
 export { FlexTemplate, FlexColumnTemplate } from './template/Template';
 
-/* Header */
+/* Nav */
 export { Nav } from './nav/Nav';
+
+/* Write */
+export { Write } from './write/Write';
+
+/* Preview */
+export { Preview } from './preview/Preview';
